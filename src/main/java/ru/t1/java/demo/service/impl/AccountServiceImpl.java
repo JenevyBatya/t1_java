02f906f82cfe1233241ee38ba74c06b631bdbaf2
Karina <thealisky@gmail.com>
2024-11-05 00:05:35 +0300

    Создание аннотации для аспекта, самого аспекта

--- src/main/java/ru/t1/java/demo/service/impl/AccountServiceImpl.java
+++ src/main/java/ru/t1/java/demo/service/impl/AccountServiceImpl.java
@@ -2,6 +2,7 @@ package ru.t1.java.demo.service.impl;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import ru.t1.java.demo.aop.LogDataSourceError;
 import ru.t1.java.demo.model.Account;
 import ru.t1.java.demo.repository.AccountRepository;
 import ru.t1.java.demo.service.AccountService;
@@ -13,21 +14,25 @@ import java.util.List;
 public class AccountServiceImpl implements AccountService {
     private final AccountRepository accountRepository;
 
+    @LogDataSourceError
     @Override
     public List<Account> findAll() {
         return accountRepository.findAll();
     }
 
+    @LogDataSourceError
     @Override
     public Account save(Account account) {
         return accountRepository.save(account);
     }
 
+    @LogDataSourceError
     @Override
     public Account findById(Long id) {
         return accountRepository.findById(id).orElse(null);
     }
 
+    @LogDataSourceError
     @Override
     public void deleteById(Long id) {
         accountRepository.deleteById(id);
--- src/main/java/ru/t1/java/demo/service/impl/TransactionServiceImpl.java
+++ src/main/java/ru/t1/java/demo/service/impl/TransactionServiceImpl.java
@@ -2,6 +2,7 @@ package ru.t1.java.demo.service.impl;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import ru.t1.java.demo.aop.LogDataSourceError;
 import ru.t1.java.demo.model.Transaction;
 import ru.t1.java.demo.repository.TransactionRepository;
 import ru.t1.java.demo.service.TransactionService;
@@ -14,21 +15,25 @@ public class TransactionServiceImpl implements TransactionService {
 
     private final TransactionRepository transactionRepository;
 
+    @LogDataSourceError
     @Override
     public List<Transaction> findAll() {
         return transactionRepository.findAll();
     }
 
+    @LogDataSourceError
     @Override
     public Transaction save(Transaction transaction) {
         return transactionRepository.save(transaction);
     }
 
+    @LogDataSourceError
     @Override
     public Transaction findById(Long id) {
         return transactionRepository.findById(id).orElse(null);
     }
 
+    @LogDataSourceError
     @Override
     public void deleteById(Long id) {
         transactionRepository.deleteById(id);
